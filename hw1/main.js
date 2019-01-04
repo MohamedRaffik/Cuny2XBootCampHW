fe5a0f73ca7f50a8060f436eaa9776777934de00
Mohamed Raffik <raffikmohamed7@gmail.com>
2019-01-03 22:58:14 -0500

    hw1

---
+++ hw1/main.js
@@ -0,0 +1,64 @@
+
+//forEach() dupe
+const myEach = (array, callback) => {
+    for (let i = 0; i < array.length; i++) {
+        callback(array[i], i, array);
+    }
+}
+
+console.log(myEach([1,2,3], (x, i) => console.log(x, i)))
+
+// map() dupe 
+const myMap = (array, callback) => {
+    let new_array = [];
+    for (let i = 0; i < array.length; i++) {
+        new_array.push(callback(array[i], i, array));
+    }
+    return new_array;
+}
+
+console.log(myMap([1,2,3], (x) => x + 2))
+
+//filter() dupe
+const myFilter = (array, callback) => {
+    let new_array = [];
+    for (let i = 0; i < array.length; i++) {
+        (callback(array[i], i, array)) ? new_array.push(array[i]) : null;
+    }
+    return new_array;
+}
+
+console.log(myFilter([1,2,3], (x) => x % 2 === 0))
+
+// some() dupe
+const mySome = (array, callback) => {
+    for (let i = 0; i < array.length; i++) {
+        if (callback(array[i], i, array)) { return true; }
+    }
+    return false;
+}
+
+console.log(mySome([1,2,3], (x) => x % 2 === 0))
+
+// every() dupe
+const myEvery = (array, callback) => {
+    for (let i = 0; i < array.length; i++) {
+        if (!callback(array[i], i, array)) { return false; }
+    }
+    return true;
+}
+
+console.log(myEvery([1,2,3], (x) => x % 2 === 0))
+
+// reduce() dupe
+const myReduce = (array, callback, start) => {
+    let start_index;
+    if (start) { total = start; start_index = 0; }
+    else { total = (array.length > 0) ? array[0] : null; start_index = 1; }
+    for (let i = start_index; i < array.length; i++) {
+        total = callback(total, array[i], i, array);
+    }
+    return total;
+}
+
+console.log(myReduce([1,2,3], (x,y) => x + y, 7));
