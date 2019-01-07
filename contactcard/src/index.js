924955a840fe25a48e10598c944f262cd9ab5925
Mohamed Raffik <raffikmohamed7@gmail.com>
2019-01-07 15:37:48 -0500

    assignment

--- contactcard/src/index.js
+++ contactcard/src/index.js
@@ -1,31 +1,42 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import PropTypes from 'prop-types';
 import './index.css';
 
-class ContactCard extends React.Component {
+class ContactCard extends React.Component {		
 	render() {
 		return (
 			<table className="card">
-				<tr>
-					<th>Name</th>
-					<td>{this.props.name}</td>
-				</tr>
-				<tr>
-					<th>Mobile Number</th>
-					<td>{this.props.mobile}</td>
-				</tr>
-				<tr>
-					<th>Work Number</th>
-					<td>{this.props.work ? this.props.work : ""}</td>
-				</tr>
-				<tr>
-					<th>Email</th>
-					<td>{this.props.email}</td>
-				</tr>
+				<tbody>
+					<tr>
+						<th>Name</th>
+						<td>{this.props.name}</td>
+					</tr>
+					<tr>
+						<th>Mobile Number</th>
+						<td>{this.props.mobile}</td>
+					</tr>
+					<tr>
+						<th>Work Number</th>
+						<td>{this.props.work ? this.props.work : ""}</td>
+					</tr>
+					<tr>
+						<th>Email</th>
+						<td>{this.props.email}</td>
+					</tr>
+				</tbody>
 			</table>
 		)
 	}
 }
+
+ContactCard.propTypes = {
+	name: PropTypes.string.isRequired,
+	mobile: PropTypes.string.isRequired,
+	work: PropTypes.string,
+	email: PropTypes.string.isRequired
+};
+
 
 class App extends React.Component {
 	render() {
